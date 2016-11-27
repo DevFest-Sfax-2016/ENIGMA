12bc87b3ab149a714d6b6495239afb6635b1dafa
mohamed amine hamdane <hamdanohamdene@gmail.com>
2016-11-27 07:20:29 +0100

    profile2

--- app/src/main/java/com/enigma/mybucketlist/Profile.java
+++ app/src/main/java/com/enigma/mybucketlist/Profile.java
@@ -1,8 +1,23 @@
 package com.enigma.mybucketlist;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Serializable;
+
 /**
  * Created by mohamed on 11/27/16.
  */
+@IgnoreExtraProperties
 
 public class Profile {
+    public String id,good,bad,change,hobbies;
+
+    public Profile(String id, String good, String bad, String change, String hobbies) {
+        this.id = id;
+        this.good = good;
+        this.bad = bad;
+        this.change = change;
+        this.hobbies = hobbies;
+
+    }
 }
