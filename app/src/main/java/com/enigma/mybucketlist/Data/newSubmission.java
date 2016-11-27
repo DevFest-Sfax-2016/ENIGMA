6cbc70d19fef1df9cfdec079f3c5dc9756988815
sensokame <justhoucem@gmail.com>
2016-11-27 11:06:32 +0100

    added submition to DB

---
+++ app/src/main/java/com/enigma/mybucketlist/Data/newSubmission.java
@@ -0,0 +1,21 @@
+package com.enigma.mybucketlist.Data;
+
+/**
+ * Created by houcem on 27/11/16.
+ */
+
+public class newSubmission {
+    public String description;
+    public String Title;
+    public String TAG;
+    public boolean anonym;
+
+    public newSubmission(String description, String title, String TAG, boolean anonym) {
+        this.description = description;
+        Title = title;
+        this.TAG = TAG;
+        this.anonym = anonym;
+    }
+
+
+}
--- app/src/main/java/com/enigma/mybucketlist/Fragments/SubmitXPFragment.java
+++ app/src/main/java/com/enigma/mybucketlist/Fragments/SubmitXPFragment.java
@@ -6,8 +6,18 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.enigma.mybucketlist.Data.newSubmission;
 import com.enigma.mybucketlist.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +25,9 @@ import com.enigma.mybucketlist.R;
 public class SubmitXPFragment extends Fragment {
 
 
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = database.getReference();
+    private View mView;
     public SubmitXPFragment() {
         // Required empty public constructor
     }
@@ -24,7 +37,27 @@ public class SubmitXPFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_submit_x, container, false);
+         mView=  inflater.inflate(R.layout.activity_submit_story, container, false);
+        Button submmit = (Button) mView.findViewById(R.id.buttonSubmit);
+        submmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                final String id =  firebaseUser.getUid();
+                String description = ((TextView)mView.findViewById(R.id.editTextStory)).getText().toString();
+                String Title = ((TextView)mView.findViewById(R.id.textViewTitle)).getText().toString();
+                String TAG =  ((Spinner)mView.findViewById(R.id.spinnerstoryCategory)).getSelectedItem().toString();
+                boolean anonym = ((CheckBox)mView.findViewById(R.id.checkBox)).isChecked();
+
+
+                newSubmission submition = new newSubmission(description,Title,TAG,anonym);
+                myRef.child("stories").child(id).setValue(submition);
+            }
+        });
+
+
+        return mView;
     }
 
 }
