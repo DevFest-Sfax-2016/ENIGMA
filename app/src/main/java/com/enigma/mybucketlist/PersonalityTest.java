01134b8c94930be031d1bb2484c507c37b39c33a
mohamed amine hamdane <hamdanohamdene@gmail.com>
2016-11-27 01:24:17 +0100

    added personality test activity

---
+++ app/src/main/java/com/enigma/mybucketlist/PersonalityTest.java
@@ -0,0 +1,13 @@
+package com.enigma.mybucketlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personality_test);
+    }
+}
