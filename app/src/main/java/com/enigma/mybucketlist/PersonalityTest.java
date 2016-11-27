3b0428c35726bbfe2056383166b787f5971e6c3a
mohamed amine hamdane <hamdanohamdene@gmail.com>
2016-11-27 01:43:00 +0100

    added personality test activity 2

--- app/src/main/java/com/enigma/mybucketlist/PersonalityTest.java
+++ app/src/main/java/com/enigma/mybucketlist/PersonalityTest.java
@@ -1,13 +1,38 @@
 package com.enigma.mybucketlist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class PersonalityTest extends AppCompatActivity {
+    private String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personality_test);
+        final Button button = (Button) findViewById(R.id.buttonTest);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.16personalities.com/free-personality-test";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        final Button button1 = (Button) findViewById(R.id.buttonNext);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Spinner s = (Spinner) findViewById(R.id.spinnerChooseType);
+                type = s.getSelectedItem().toString();
+            }
+        });
     }
+
 }
